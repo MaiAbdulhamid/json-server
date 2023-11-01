0a8e0287d300c3aee2e1f67a4029e841d64c110f
maiAbdulhameed <mai.abdelhameed@rowaad.net>
2023-11-01 22:22:59 +0200

    Changes

--- api/server.ts
+++ api/server.ts
@@ -5,14 +5,10 @@ const server = jsonServer.create()
 // Comment out to allow write operations
 const router = jsonServer.router('db.json')
 
-const middlewares = jsonServer.defaults()
+// const middlewares = jsonServer.defaults()
 
-server.use(middlewares)
-// Add this before server.use(router)
-// server.use(jsonServer.rewriter({
-//     '/api/*': '/$1',
-//     '/properties/:id': '/:id'
-// }))
+// server.use(middlewares)
+
 server.use(router)
 server.listen(3000, () => {
   console.log('JSON Server is running')
